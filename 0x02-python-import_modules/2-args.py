fd06aadea6eb659644507a0f44f3fe049b4b946a
Mukwiza <itresor86@gmail.com>
2022-06-30 23:55:28 +0200

    script dyanmic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("{} argument:".format(n))
     else:
         print("{} arguments:".format(n))
-        for i in range(1, n+1):
+        for i in range(len(n+1)):
             print("{}: {}".format(i, sys.argv[i]))
