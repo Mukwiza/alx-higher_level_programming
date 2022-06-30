78b98fedfcf664568517187f6967eb572b0624ba
Mukwiza <itresor86@gmail.com>
2022-07-01 00:05:42 +0200

    script dyanmic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     n = len(sys.argv) - 1
     if n == 0:
-        print("{} arguments.".format(n))
+        print("0 arguments.")
     elif n == 1:
-        print("{} argument:".format(n))
+        print("1 argument:")
     else:
         print("{} arguments:".format(n))
-        for i in range(n):
+        for i in range(1, n+1):
             print("{}: {}".format(i, sys.argv[i]))
