35c5943fbc0d12d415f0699bbe5797731fffa6e4
Mukwiza <itresor86@gmail.com>
2022-07-01 02:24:02 +0200

    100-calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,11 @@ if __name__ = "__main__":
     n = len(sys.argv)
     if n != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         operator = sys.argv[2]
-        sys.exit(1)
         if operator == '+':
                 print("{} + {} = {}".format(a, b, add(a, b)))
         elif operator == '-':
