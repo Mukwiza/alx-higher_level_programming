31b5c778b39e49ddf244ac6cc792be8e772a6874
Mukwiza <itresor86@gmail.com>
2022-07-05 16:17:40 +0200

    remove_cC

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    string = ""
+    for i in my_string:
+        if i!= chr(99) and i!= chr(67):
+          string += i
+    return string
+
+
+
---
+++ 0x03-python-data_structures/test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+a = __import__('5-no_c').no_c
+
+print(a("best school is Cool"))
