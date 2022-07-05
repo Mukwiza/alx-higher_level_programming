0adf66c8cb85e875fbee301aeb88d1d1b0c57742
Mukwiza <itresor86@gmail.com>
2022-07-05 16:22:00 +0200

    remove_cC

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    string = ""
+    string = ''
     for i in my_string:
-        if i != chr(99) and i != chr(67):
-          string += i
-    return string
+        if i != 'c' and i != 'C':
+            string += i
+
+        return string
