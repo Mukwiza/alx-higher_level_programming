d4dcad511a18a62f841e62cc404e918361701582
Mukwiza <itresor86@gmail.com>
2022-07-07 22:41:37 +0200

    best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if  a_dictionary is None:
+    if a_dictionary is None:
         return None
 
-    x = max(a_dictionary.values())
+    b_score = max(a_dictionary.values(), default=None)
     for k, v in a_dictionary.items():
-        if v == x:
+        if v == b_score:
             return k
