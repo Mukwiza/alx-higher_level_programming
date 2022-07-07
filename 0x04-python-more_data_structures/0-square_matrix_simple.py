e02df3ce4109e06fba5bfec943a819819c424d6f
Mukwiza <itresor86@gmail.com>
2022-07-07 17:08:59 +0200

    squared

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     n_matrix = []
     if len(matrix) > 0:
         for row in matrix:
-                n_matrix.append(list(map(lambda x: x**2, row)))
+            n_matrix.append(list(map(lambda x: x**2, row)))
     return n_matrix
