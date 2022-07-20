71daabfd0293e00c5db9379e201c92a32ef0a0d8
Mukwiza <itresor86@gmail.com>
2022-07-20 22:52:06 +0200

    getter_setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,23 +17,23 @@ class Square:
         self.__size = size
 
     @property
-
     def size(self):
         """Retrieve size."""
 
         return self.__size
 
     @size.setter
-
     def size(self, value):
        """Set the size."""
+
        if not isinstance(value, int):
            raise TypeError("size must be an integer")
        elif value < 0:
            raise ValueError("size must be >= 0")
-       self.__size = value
+       else:
+           self.__size = value
 
-       def area(self):
-           """Return the current square area."""
+     def area(self):
+         """Return the current square area."""
 
-           return self.__size ** 2
+         return self.__size ** 2
