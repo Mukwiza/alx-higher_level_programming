78d81de00d97fa4853dfbbb0b910bc8da54f79f2
Mukwiza <itresor86@gmail.com>
2022-08-02 00:05:10 +0200

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,11 +2,11 @@
 """Module: 4-inherits_from."""
 
 def inherits_from(obj, a_class):
-    """function: inherits_from.
+    """Class inherits from parent class.
     Args:
         obj: an object to check.
         a_class: a class.
     Returns:
-        None.
+        None
     """
     return type(obj) != a_class and isinstance(obj, a_class)
