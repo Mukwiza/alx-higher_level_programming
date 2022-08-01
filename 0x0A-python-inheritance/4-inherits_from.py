0303078f64677ea13fbf4fd5ac09686f7970d326
Mukwiza <itresor86@gmail.com>
2022-08-02 00:01:09 +0200

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,14 +2,13 @@
 """module: 4-inherits_from
 """
 
-
 def inherits_from(obj, a_class):
     """function: inherits_from.
     Args:
         obj: an object to check.
         a_class: a class.
     Returns:
-        bool
+        bool.
     """
     if type(obj) != a_class and isinstance(obj, a_class):
             return True
