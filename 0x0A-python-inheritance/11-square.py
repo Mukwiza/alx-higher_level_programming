d92ad417d25e70de585513116fa7c61eac6a0e83
Mukwiza <itresor86@gmail.com>
2022-08-01 23:38:38 +0200

    square2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Implementing a Geometry class."""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Represents Square class."""
+
+    def __init__(self, size):
+        """initialize data."""
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Print."""
+        return ("[Rectangle] " + str(self.__size) + "/" + str(self.__size))
