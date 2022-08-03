05c1fc595921e9d38f2ecbd10ed66d640b8e757a
Mukwiza <itresor86@gmail.com>
2022-08-03 16:24:39 +0200

    json3

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines function that creates an object form a json file"""
+
+
+def load_from_json_file(filename):
+    """creates an object
+    Args:
+        filename: name of a json file.
+    Returns:
+        None.
+    """
+    import json
+    with open(filename, 'r', encoding='utf-8') as f:
+        json.load(f)
