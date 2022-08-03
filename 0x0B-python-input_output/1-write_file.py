a25823b7486f32e42ac5223976669218a2a2b92f
Mukwiza <itresor86@gmail.com>
2022-08-03 13:33:26 +0200

    write

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Function that writes a string to a text file."""
+
+
+def write_file(filename="", text=""):
+    """write a string to text file and prints it to stdout.
+    Args:
+        filename = name of a file
+        text = a string to write in text file.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+      return f.write(text)
