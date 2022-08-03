df63e7f4842ca3b12644eeed4aaf4f9016b91d97
Mukwiza <itresor86@gmail.com>
2022-08-03 13:15:34 +0200

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,8 @@ def read_file(filename=""):
     """Opening and reading file.
     Args:
         filename (string): name of a file.
+    Return:
+        None.
     """
     with open(filename, 'r', encoding='utf-8') as f:
        print(f.read(), end='')
