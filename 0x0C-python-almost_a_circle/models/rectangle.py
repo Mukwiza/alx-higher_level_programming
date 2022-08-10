322c3d5e631a1388b5802bd0c2d9e1d32cdf168f
Mukwiza <itresor86@gmail.com>
2022-08-10 20:48:35 +0200

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,61 +1,80 @@
 #!/usr/bin/python3
-'''Class Rectangle.'''
+'''
+    Class Rectangle
+'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''Defining rectangle class inherits from base.
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
     '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
     def width(self):
-        '''returning private attribute.'''
-
+        '''
+            Returning private attribute
+        '''
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''setting private attribute.'''
-        self.setter_validation('width', value)
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
         self.__width = value
 
     @property
     def height(self):
-        '''returning private attribute.'''
+        '''
+            Returning private attribute
+        '''
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''setting private attribute.'''
-        self.setter_validation('height', value)
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
         self.__height = value
 
     @property
     def x(self):
-        '''returning private attribute.'''
+        '''
+            Returning private attribute
+        '''
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''setting private attribute.'''
-        self.setter_validation('x', value)
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("x", value)
         self.__x = value
 
     @property
     def y(self):
-        '''returning private attribute.'''
+        '''
+            Returning private attribute
+        '''
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''setting private attribute.'''
-        self.setter_validation('y', value)
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("y", value)
         self.__y = value
-
