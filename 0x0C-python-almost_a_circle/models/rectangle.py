533d920a222ef20658f41d21b18d2e723120991f
Mukwiza <itresor86@gmail.com>
2022-08-10 20:53:15 +0200

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,80 +1,72 @@
 #!/usr/bin/python3
-'''
-    Class Rectangle
-'''
+"""
+This module implements a Rectangle object
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''
-        Defining the Rectangle class
-        Inherits from:
-            Base
-    '''
+    """Rectangle implementation
+    """
 
-    def __init__(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width: int, height: int, x=0, y=0, id=None):
+        """initialization
+        """
+        super().__init__(id)
+
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
-    def width(self):
-        '''
-            Returning private attribute
-        '''
+    def width(self) -> int:
+        """width getter
+        """
         return self.__width
 
     @width.setter
-    def width(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("width", value)
-        self.__width = value
+    def width(self, width: int):
+        """width setter
+        """
+        self.check_type_value('width', width)
+        self.__width = width
 
     @property
-    def height(self):
-        '''
-            Returning private attribute
-        '''
+    def height(self) -> int:
+        """height getter
+        """
         return self.__height
 
     @height.setter
-    def height(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("height", value)
-        self.__height = value
+    def height(self, height: int):
+        """height setter
+        """
+        self.check_type_value('height', height)
+        self.__height = height
 
     @property
-    def x(self):
-        '''
-            Returning private attribute
-        '''
+    def x(self) -> int:
+        """x getter
+        """
         return self.__x
 
     @x.setter
-    def x(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("x", value)
-        self.__x = value
+    def x(self, x: int):
+        """x setter
+        """
+        self.check_type_value('x', x, True)
+        self.__x = x
 
     @property
-    def y(self):
-        '''
-            Returning private attribute
-        '''
+    def y(self) -> int:
+        """y getter
+        """
         return self.__y
 
     @y.setter
-    def y(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("y", value)
-        self.__y = value
+    def y(self, y: int):
+        """y setter
+        """
+        self.check_type_value('y', y, True)
+        self.__y =
