373a3e3fc8887f69c5c4574667c348286d6ee06d
Mukwiza <itresor86@gmail.com>
2022-08-11 13:18:57 +0200

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,4 +94,4 @@ class Rectangle(Base):
 
     def __str__(self):
         '''retuns a string'''
-        return ('[Rectangle]  ({}) {}/{ - {}/{}'.format(self.id, self.x, self.y, self.width, self.height))
+        return ('[Rectangle]  ({}) {}/{} - {}/{}'.format(self.id, self.x, self.y, self.width, self.height))
