f66494222f9791e58f57242c8f66a0e17286df5d
Mukwiza <itresor86@gmail.com>
2022-08-12 18:13:57 +0200

    args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -45,7 +45,7 @@ class Square(Rectangle):
             *args (int): non keyworded arguments
             **kwargs (int): keyworded arguments
         '''
-        if args and len(args) != 0:
+        if (args):
             for i, l in enumerate(args):
                 if i == 0:
                     self.id = l
@@ -56,6 +56,6 @@ class Square(Rectangle):
                 elif l == 3:
                     self.y = l
 
-        if kwargs and len(kwargs) != 0:
+        if (kwargs):
             for key, value in kwargs.items():
                 setattr(self, key, value)
