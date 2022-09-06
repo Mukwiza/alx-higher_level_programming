d1e3d5838ecef1d03bb8faf3a454df392266a504
Mukwiza <itresor86@gmail.com>
2022-09-06 15:55:09 +0200

    count

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Script that displays the number of records of id in  first_table of the database in MySQL server.
-SELECT COUNT(id) FROM first_table;
+SELECT COUNT(id) FROM first_table WHERE id = 89;
